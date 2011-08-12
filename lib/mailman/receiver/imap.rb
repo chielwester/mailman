1a06a80a4e1e7bc593057379f62782b463b02822
Chiel Wester <chiel.wester@holder.nl>
2011-08-12 11:45:40 +0200

    Add the imap receiver

--- lib/mailman/receiver/imap.rb
+++ lib/mailman/receiver/imap.rb
@@ -1,12 +1,12 @@
 require 'net/imap'
 
 module Mailman
-  class Receiver
+  module Receiver
     # Receives messages using IMAP, and passes them to a {MessageProcessor}.
     class IMAP
 
       # @return [Net::IMAP] the IMAP connection
-      attr_reader :connection
+      attr_reader :connection, :username, :password, :server
 
       # @param [Hash] options the receiver options
       # @option options [MessageProcessor] :processor the processor to pass new
@@ -16,20 +16,29 @@ module Mailman
       # @option options [String] :username the username to authenticate with
       # @option options [String] :password the password to authenticate with
       def initialize(options)
-	      @processor, @username, @password, @server, @filter, @port = nil, nil, nil, nil, ["NEW"], 143
+        @processor, @username, @password, @server, @filter, @port = nil, nil, nil, nil, ["NEW"], 143
 
         @processor = options[:processor] if options.has_key? :processor
         @username =  options[:username]  if options.has_key? :username
         @password =  options[:password]  if options.has_key? :password
         @filter =    options[:filter]    if options.has_key? :filter
         @port =      options[:port]      if options.has_key? :port
-        @connection = Net::IMAP.new(options[:server], @port)
+        @options = options
+        @server = options[:server]       if options.has_key? :server
+        @use_ssl = options[:ssl]||false
       end
 
       # Connects to the IMAP server.
       def connect
-        @connection.login(@username, @password)
-	      @connection.examine("INBOX")
+        @connection = Net::IMAP.new(@options[:server], :port => @port, :ssl => @use_ssl)
+
+        Mailman.logger.info "CONNECTING TO #{@server}, #{@username} #{@password}"
+        @connection.authenticate(@options[:authenticate_with]||'LOGIN', @username, @password)
+        unless @connection.list('', '%').any?{|mailbox| mailbox.name == 'Processed'}
+          @connection.create('Processed')
+        end
+        Mailman.logger.info "Connection established"
+        @connection.select 'INBOX'
       end
 
       # Disconnects from the IMAP server.
@@ -40,12 +49,15 @@ module Mailman
       # Iterates through new messages, passing them to the processor, and
       # deleting them.
       def get_messages
-        @connection.search(@filter).each do  |message| 
+        @connection.uid_search(@filter).each do |message|
           puts "PROCESSING MESSAGE #{message}"
-          body=@connection.fetch(message,"RFC822")[0].attr["RFC822"]
-          @processor.process(body)
-	        @connection.store(message,"+FLAGS",[:Seen])
+          body=@connection.uid_fetch(message,"RFC822")[0].attr["RFC822"]
+          @processor.process(body, @options)
+          @connection.uid_copy(message, 'Processed')
+
+          @connection.uid_store(message,"+FLAGS",[:Deleted])
         end
+        @connection.expunge
         #@connection.delete_all
       end
 
